d8661e4c55d31105fb6e22a6278ee477b17c817b
Ivan Patsai <patsai.ivan@gmail.com>
2017-01-24 00:47:06 +0200

    First commit

---
+++ script.js
@@ -0,0 +1,48 @@
+var uchoice = function (){
+  var userChoice = prompt("Do you choose rock, paper or scissors?");
+  return userChoice;
+};
+var cchoice = function (){
+  var computerChoice = Math.random();
+  if (computerChoice < 0.34) {
+    computerChoice = "rock";
+  } else if(computerChoice <= 0.67) {
+    computerChoice = "paper";
+  } else {
+    computerChoice = "scissors";
+  } console.log("Computer: " + computerChoice);
+  return computerChoice;
+};
+var compare = function ( choice1, choice2){
+  if (choice1 === choice2) {
+    console.log("The result is a draw!");
+    console.log("One more time!");
+    //if draw call compare function again
+    return compare (uchoice(), cchoice());
+  } else if ( choice1==="rock"){
+    if (choice2==="scissors"){
+      return "rock wins";
+    }
+    else {
+      return "paper wins";
+    }
+  }
+  if (choice1 === "paper"){
+    if (choice2 ==="rock"){
+      return "paper wins";
+    } else {
+      return "scissors wins";
+    }
+  }
+  if (choice1 === "scissors"){
+    if (choice2 ==="rock"){
+      return "rock wins";
+    } else {
+      return "scissors wins";
+    }
+  }
+};
+alert(compare (uchoice(), cchoice()));
+
+
+
